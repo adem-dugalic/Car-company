625df7888eda87f0d09edec65e82d5a0b1c0040e
skeri123 <skeri123@users.noreply.github.com>
2019-11-22 18:28:55 +0100

    Update

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var fade = document.getElementById('top');
+
+window.addEventListener('scroll', function(){
+	fade.style.opacity = 1- +window.pageYOffset/550+'';
+	fade.style.top = +window.pageYOffset+'px';
+	fade.style.backgroundPositionY = - +window.pageYOffset/2+'px';
+});
+
+
+var menu = document.querySelector('.menu');
+var nav = document.querySelector('.links');
+var navLinks = document.querySelectorAll('.links li');
+
+menu.addEventListener('click', function(){
+
+	nav.classList.toggle('navigation-active');
+	navLinks.forEach((link,index)=>{
+		if(link.style.animation){
+		link.style.animation = '';
+		}else{
+			link.style.animation = `Fade 0.5s ease forwards ${index/7+0.6 }s`;
+		}
+	});
+
+	menu.classList.toggle('toggle');
+
+});
